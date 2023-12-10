1fb4d2a694bc77f70e87e3d511289e1b98377259
Quinten Rutgers <quinten.rutgers@hotmail.nl>
2023-12-10 23:31:01 +0100

    Added keywords, identifiers, whitespace

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -3,10 +3,10 @@ package lexer
 import "github.com/QRCoding/monkey/token"
 
 type Lexer struct {
-	input		 string
-	position	 int  // current position in input (points to current char)
+	input        string
+	position     int  // current position in input (points to current char)
 	readPosition int  // current reading position in input (after current char)
-	ch			 byte // current char under examination
+	ch           byte // current char under examination
 }
 
 func New(input string) *Lexer {
@@ -28,6 +28,8 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -48,6 +50,15 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
@@ -56,4 +67,22 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
 }
