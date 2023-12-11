9bc871a23fef25beb1ca0f89fa4b51a5de976e9b
Quinten Rutgers <quintenrutgers@upcmail.nl>
2023-12-11 18:35:49 +0100

    Added string test

--- ast/ast.go
+++ ast/ast.go
@@ -25,6 +25,14 @@ type Program struct {
 }
 
 func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (p *Program) String() string {
 	var out bytes.Buffer
 
 	for _, s := range p.Statements {
