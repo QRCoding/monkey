903e33464b22f99df4f72cb3cd3d5f72d35432c9
Quinten Rutgers <quintenrutgers@upcmail.nl>
2023-12-11 13:55:49 +0100

    Added AST interfaces

---
+++ ast/ast.go
@@ -0,0 +1,18 @@
+package ast
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+}
